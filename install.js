84651e96c01547fe1ae038f867072f04c81ad9eb
pavel <konon.pavel@gmail.com>
2015-12-21 09:26:47 +0300

    - Fix case when vague file exists

--- install.js
+++ install.js
@@ -3,4 +3,7 @@ var fs = require('fs');
 
 var staticFolderPath = 'subsystems/webui/static';
 var vaguePath = path.relative(path.join(__dirname, staticFolderPath), require.resolve('vague-time'));
-fs.symlinkSync(vaguePath, path.join(staticFolderPath, 'vagueTime.js'));
+var newVaguePath = path.join(staticFolderPath, 'vagueTime.js');
+if (!fs.existsSync(newVaguePath)) {
+    fs.symlinkSync(vaguePath, newVaguePath);
+}
